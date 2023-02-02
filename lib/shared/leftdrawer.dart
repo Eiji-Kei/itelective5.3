f821cb7c486e041c80d8ebb1e78de0ab7b1eefba
Eiji-Kei <kristineangelagallawan9@gmail.com>
2023-02-02 08:37:50 +0800

    modified:   lib/shared/leftdrawer.dart 	modified:   lib/shared/rightdrawer.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/shared/leftdrawer.dart
+++ lib/shared/leftdrawer.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unrelated_type_equality_checks
 
 import 'package:flutter/material.dart';
 import 'package:ite5_2022/screens/about.dart';
+import 'package:ite5_2022/screens/home.dart';
 
 class LeftDrawer extends StatefulWidget {
   const LeftDrawer({super.key});
@@ -13,94 +14,93 @@ class LeftDrawer extends StatefulWidget {
 class _LeftDrawerState extends State<LeftDrawer> {
   @override
   Widget build(BuildContext context) {
-    bool yesno = false;
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width > 600
           ? MediaQuery.of(context).size.width * .33
           : MediaQuery.of(context).size.width * .5,
       color: Color.fromARGB(255, 113, 164, 236),
-        child: ListView(
-          padding: EdgeInsets.all(24),
-          children: [
-            SizedBox(height: 20,),
-            Container(
-              color: Color.fromARGB(255, 73, 139, 231),
-              child: ListTile(
-                textColor: Colors.white,
-                iconColor: Colors.white,
-                shape: RoundedRectangleBorder(side: BorderSide(width: 1)),
-                leading: Icon(
-                  Icons.home,
-                ),
-                title: Text('Home'),
-                onTap: () => 'Null',
+      child: ListView(
+        padding: EdgeInsets.all(24),
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            color: Color.fromARGB(255, 73, 139, 231),
+            child: ListTile(
+              textColor: Colors.white,
+              iconColor: Colors.white,
+              shape: RoundedRectangleBorder(side: BorderSide(width: 1)),
+              leading: Icon(
+                Icons.home,
               ),
+              title: Text('Home'),
+              onTap: () => 'Null',
             ),
-            
-            Container(
-              color: Color.fromARGB(255, 73, 139, 231),
-              child: ListTile(
-                textColor: Colors.white,
-                iconColor: Colors.white,
-                shape: RoundedRectangleBorder(side: BorderSide(width: 1)),
-                leading: Icon(
-                  Icons.work,
-                ),
-                title: Text('Projects'),
-                onTap: () => 'Null',
+          ),
+          Container(
+            color: Color.fromARGB(255, 73, 139, 231),
+            child: ListTile(
+              textColor: Colors.white,
+              iconColor: Colors.white,
+              shape: RoundedRectangleBorder(side: BorderSide(width: 1)),
+              leading: Icon(
+                Icons.work,
               ),
+              title: Text('Projects'),
+              onTap: () => 'Null',
             ),
-            Container(
-              color: Color.fromARGB(255, 73, 139, 231),
-              child: ListTile(
-                textColor: Colors.white,
-                iconColor: Colors.white,
-                shape: RoundedRectangleBorder(side: BorderSide(width: 1)),
-                leading: Icon(
-                  Icons.file_copy,
-                ),
-                title: Text('Reports'),
-                onTap: () => 'Null',
+          ),
+          Container(
+            color: Color.fromARGB(255, 73, 139, 231),
+            child: ListTile(
+              textColor: Colors.white,
+              iconColor: Colors.white,
+              shape: RoundedRectangleBorder(side: BorderSide(width: 1)),
+              leading: Icon(
+                Icons.file_copy,
               ),
+              title: Text('Reports'),
+              onTap: () => 'Null',
             ),
-            SizedBox(
-              height: 350,
-            ),
-            Container(
-              color: Color.fromARGB(255, 73, 139, 231),
-              child: ListTile(
-                textColor: Colors.white,
-                iconColor: Colors.white,
-                shape: RoundedRectangleBorder(side: BorderSide(width: 1)),
-                leading: Icon(
-                  Icons.settings,
-                ),
-                title: Text('Settings'),
-                onTap: () => 'Null',
+          ),
+          SizedBox(
+            height: 350,
+          ),
+          Container(
+            color: Color.fromARGB(255, 73, 139, 231),
+            child: ListTile(
+              textColor: Colors.white,
+              iconColor: Colors.white,
+              shape: RoundedRectangleBorder(side: BorderSide(width: 1)),
+              leading: Icon(
+                Icons.settings,
               ),
+              title: Text('Settings'),
+              onTap: () => 'Null',
             ),
-
-            Container(
-              color: Color.fromARGB(255, 73, 139, 231),
-              child: ListTile(
-                textColor: Colors.white,
-                iconColor: Colors.white,
-                shape: RoundedRectangleBorder(side: BorderSide(width: 1)),
-                leading: Icon(
-                  Icons.info,
-                ),
-                title: Text('About Us'),
-                onTap: () {
-                  Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const AboutUs()));
-                },
+          ),
+          Container(
+            color: Color.fromARGB(255, 73, 139, 231),
+            child: ListTile(
+              textColor: Colors.white,
+              iconColor: Colors.white,
+              shape: RoundedRectangleBorder(side: BorderSide(width: 1)),
+              leading: Icon(
+                Icons.info,
               ),
+              title: Text('About Us'),
+              onTap: () {
+                if (Home() == true) {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const AboutUs()));
+                } else {}
+              },
             ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/shared/rightdrawer.dart
+++ lib/shared/rightdrawer.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:ite5_2022/screens/login.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class RightDrawer extends StatefulWidget {
   const RightDrawer({super.key});
@@ -24,149 +25,142 @@ class _RightDrawerState extends State<RightDrawer> {
           padding: EdgeInsets.all(24),
           children: [
             SizedBox(
-              height: 380,
+              height: 400,
               child: Card(
                 elevation: 50,
                 shadowColor: Colors.white,
                 shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30)),
+                    borderRadius: BorderRadius.circular(30)),
                 child: Column(
-              children: [
-                Center(
-                  child: Stack(
-                    children: <Widget>[
-                      Image.asset(
-                        'me.png',
-                        width: 60,
-                        height: 60,
+                  children: [
+                    Center(
+                      child: Stack(
+                        children: <Widget>[
+                          Image.asset(
+                            'me.png',
+                            width: 60,
+                            height: 60,
+                          ),
+                          Positioned(
+                            bottom: 5,
+                            right: 5,
+                            child: Image.asset(
+                              'check.png',
+                              width: 15,
+                              height: 15,
+                            ),
+                          )
+                        ],
                       ),
-                      Positioned(
-                        bottom: 5,
-                        right: 5,
-                        child: Image.asset(
-                          'check.png',
-                          width: 15,
-                          height: 15,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Text(
-                  'Gallawan, Kristine Angela A.',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.black,
-                  ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                const Text(
-                  'Slytherin',
-                  style: TextStyle(
-                    fontSize: 9,
-                    color: Color.fromARGB(255, 125, 222, 252),
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    socialcount('Projects     Following     Followers'),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    socialnum('11          246          135'),
-                  
-                  ],
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                const Text(
-                  ' Lorem ipsum dolor sit amet, consectetur adip iscing elit. Sed et est et velit ornare ultrices. Ut vitae diam volutpatrisus eget. ',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 9,
-                    color: Color.fromARGB(255, 125, 222, 252),
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                SizedBox(
-                  width: 170,
-                  height: 50,
-                  child: Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: Row(
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const AutoSizeText(
+                      'Gallawan, Kristine Angela A.',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const AutoSizeText(
+                      'Slytherin',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 125, 222, 252),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        IconButton(
-                            onPressed: () => 'Null',
-                            icon: Icon(Icons.facebook)),
-                        IconButton(
-                            onPressed: () => 'Null', icon: Icon(Icons.abc)),
-                        IconButton(
-                            onPressed: () => 'Null',
-                            icon: Icon(Icons.backpack)),
-                        IconButton(
-                            onPressed: () => 'Null',
-                            icon: Icon(Icons.cached_rounded)),
+                        socialcount('Projects     Following     Followers'),
                       ],
                     ),
-                  ),
-                ),
-                
-              ],
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        socialnum('101             246             135'),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    AutoSizeText(
+                      ' Lorem ipsum dolor sit amet, consectetur adip iscing elit. Sed et est et velit ornare ultrices. Ut vitae diam volutpatrisus eget. ',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 9,
+                        color: Color.fromARGB(255, 125, 222, 252),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SizedBox(
+                      width: 170,
+                      height: 50,
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Row(
+                          children: [
+                            IconButton(
+                                onPressed: () => 'Null',
+                                icon: Icon(Icons.facebook)),
+                            IconButton(
+                                onPressed: () => 'Null', icon: Icon(Icons.abc)),
+                            IconButton(
+                                onPressed: () => 'Null',
+                                icon: Icon(Icons.backpack)),
+                            IconButton(
+                                onPressed: () => 'Null',
+                                icon: Icon(Icons.cached_rounded)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
             const SizedBox(
-                      height: 200,
-                    ),
+              height: 200,
+            ),
             Container(
-                      color: Color.fromARGB(255, 73, 139, 231),
-                      child: ListTile(
-                        textColor: Colors.white,
-                        iconColor: Colors.white,
-                        shape:
-                            RoundedRectangleBorder(side: BorderSide(width: 1)),
-                        leading: Icon(
-                          Icons.exit_to_app,
-                        ),
-                        title: Text('Log Out'),
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const LoginScreen()));
-                        },
-                      ),
-                    ),
+              color: Color.fromARGB(255, 73, 139, 231),
+              child: ListTile(
+                textColor: Colors.white,
+                iconColor: Colors.white,
+                shape: RoundedRectangleBorder(side: BorderSide(width: 1)),
+                leading: Icon(
+                  Icons.exit_to_app,
+                ),
+                title: Text('Log Out'),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginScreen()));
+                },
+              ),
+            ),
           ],
-          
         ),
       ),
     );
   }
 
-  Text socialnum(String caption) {
-    return Text(
+  AutoSizeText socialnum(String caption) {
+    return AutoSizeText(
       caption,
       textAlign: TextAlign.center,
       style: TextStyle(
-        fontSize: 12,
         color: Colors.black,
       ),
     );
@@ -177,9 +171,7 @@ class _RightDrawerState extends State<RightDrawer> {
       caption,
       textAlign: TextAlign.center,
       style: const TextStyle(
-        fontSize: 10,
-        color: Color.fromARGB(255, 125, 222, 252),
-      ),
+          color: Color.fromARGB(255, 125, 222, 252), fontSize: 13),
     );
   }
 }
